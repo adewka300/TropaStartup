da9a3975840e172933d6fef52c58d23b34e74978
adewka300 <109631649+adewka300@users.noreply.github.com>
2026-06-16 15:40:57 +0300

    upd route preview

--- src/pages/route/sections/places/components/RoutePlaceDetailsCardContainer.tsx
+++ src/pages/route/sections/places/components/RoutePlaceDetailsCardContainer.tsx
@@ -2,12 +2,16 @@
 import type { PlaceDetails } from '@/entities/place'
 import PlaceDetailsCard from '@/entities/place/ui/PlaceDetailsCard'
 
+const DEFAULT_PLACE_IMAGE = 'place.jpg';
+
 interface RoutePlaceDetailsCardContainerProps {
     place: PlaceDetails
     onCardClick?: (placeId: string) => void
 }
 
 export default function RoutePlaceDetailsCardContainer({ place, onCardClick }: RoutePlaceDetailsCardContainerProps) {
+    const imageUrl = place.image || DEFAULT_PLACE_IMAGE;
+    
     return (
         <div className="flex flex-col gap-6 w-full">
             <PlaceDetailsCard
@@ -18,7 +22,7 @@ export default function RoutePlaceDetailsCardContainer({ place, onCardClick }: R
                 reviewsCount={place.reviewsCount}
                 rating={place.rating}
                 verifiedDate={place.verifiedDate}
-                image={place.image}
+                image={imageUrl}
                 badgeTypes={place.badgeTypes}
                 city={place.city}
                 button={{
